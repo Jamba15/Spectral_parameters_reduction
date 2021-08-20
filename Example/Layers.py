b2ea48ff01c5aba48274187394a4ef6a6dc0fb3f
Jamba15 <lorenzo.giambagli@gmail.com>
2021-08-20 12:09:20 +0200

    added sparse layers

--- Example/Layers.py
+++ Example/Layers.py
@@ -1,9 +1,13 @@
 from tensorflow.python.keras.engine.base_layer import Layer
 from tensorflow.python.keras import activations, initializers, regularizers, constraints
+from tensorflow.python.keras.layers import Dense
 from tensorflow.python.util.tf_export import keras_export
 from tensorflow.python.ops.gen_math_ops import mul, mat_mul
 from tensorflow.python.ops.linalg.linalg import svd, transpose, qr
-from tensorflow import reshape
+from tensorflow.python.keras.layers.ops.core import dense as dense_action
+from tensorflow import reshape, where, abs, function
+import tensorflow_probability as tfp
+
 """
 
 Implementation of a Spectral + SVD layer that can learn eigenvalues of the associated adjacency matrix and of its SVD.
@@ -11,6 +15,11 @@ Implementation of a Spectral + SVD layer that can learn eigenvalues of the assoc
 @authors: Lorenzo Chicchi, Lorenzo Buffoni, Lorenzo Giambagli
 
 """
+
+
+@function(experimental_relax_shapes=True)
+def sparsify(x, c):
+    return where(abs(x) > c, x, 0)
 
 
 class MatrixInitializer(initializers.Initializer):
@@ -344,3 +353,190 @@ class SSVD(Layer):
             action = mat_mul(inputs, transpose(lin))
         return self.activation(action)
 
+
+@keras_export('keras.layers.QR')
+class QR_sparse(Layer):
+    def __init__(self,
+                 units,
+                 activation=None,
+                 is_eig_in_trainable=True,
+                 is_eig_out_trainable=True,
+                 use_bias=False,
+                 eig_in_initializer='ones',
+                 eig_out_initializer='zeros',
+                 svd_initializer='optimized_uniform',
+                 bias_initializer='zeros',
+                 eig_in_regularizer=None,
+                 eig_out_regularizer=None,
+                 bias_regularizer=None,
+                 activity_regularizer=None,
+                 eig_in_constraint=None,
+                 eig_out_constraint=None,
+                 bias_constraint=None,
+                 phi_mean=0,
+                 phi_stddev=0.036,
+                 eig_in=0.97,
+                 eig_out=0.13,
+                 percentile=100,
+                 dynamic_sparse=False,
+                 **kwargs):
+
+        super(QR_sparse, self).__init__(
+            activity_regularizer=activity_regularizer,
+            **kwargs)
+
+        self.units = int(units) if not isinstance(units, int) else units
+        self.activation = activations.get(activation)
+        self.is_eig_in_trainable = is_eig_in_trainable
+        self.is_eig_out_trainable = is_eig_out_trainable
+        self.use_bias = use_bias
+        self.perc = percentile
+        # 'optimized_uniform' initializers optmized by Buffoni and Giambagli
+
+        if svd_initializer == 'optimized_uniform':
+            self.svd_initializer = initializers.random_normal(mean=phi_mean,
+                                                              stddev=phi_stddev)  # (mean=0, stddev=0.3)#RandomUniform(-0.01, 0.01)#(-0.5, 0.5)
+        else:
+            self.svd_initializer = initializers.get(svd_initializer)
+
+        if eig_in_initializer == 'optimized_uniform':
+            self.eig_in_initializer = initializers.RandomUniform(-eig_in, eig_in)  # (-0.01, 0.01)
+        else:
+            self.eig_in_initializer = initializers.get(eig_in_initializer)
+
+        if eig_out_initializer == 'optimized_uniform':
+            self.eig_out_initializer = initializers.RandomUniform(-eig_out, eig_out)  # (-0.0001, 0.0001)
+        else:
+            self.eig_out_initializer = initializers.get(eig_out_initializer)
+
+        self.bias_initializer = initializers.get(bias_initializer)
+
+        self.eig_in_regularizer = regularizers.get(eig_in_regularizer)
+
+        self.eig_out_regularizer = regularizers.get(eig_out_regularizer)
+
+        self.bias_regularizer = regularizers.get(bias_regularizer)
+
+        self.eig_in_constraint = constraints.get(eig_in_constraint)
+
+        self.eig_out_constraint = constraints.get(eig_out_constraint)
+
+        self.bias_constraint = constraints.get(bias_constraint)
+        self.dynamic_sparse = dynamic_sparse
+
+    def build(self, input_shape):
+
+        # eigenvector matrix to be decomposed with QR
+        base = self.svd_initializer(shape=(self.units, input_shape[-1]))
+        q_debug, r_debug = qr(transpose(base), full_matrices=False)
+
+        self.control = tfp.stats.percentile(abs(base),
+                                            self.perc)  # tf.constant(tf.reduce_mean(tf.abs(base),axis=(0,1)))
+        self.q_debug = transpose(q_debug)
+        self.r_debug = transpose(r_debug)
+
+        self.q = self.add_weight(
+            name='Q',
+            shape=self.q_debug.shape,
+            initializer=MatrixInitializer(self.q_debug),  # SVDInitializer(u),
+            regularizer=None,
+            constraint=None,
+            dtype=self.dtype,
+            trainable=False
+        )
+
+        self.r = self.add_weight(
+            name='R',
+            shape=self.r_debug.shape,
+            initializer=MatrixInitializer(self.r_debug),  # SVDInitializer(u),
+            regularizer=None,
+            constraint=None,
+            dtype=self.dtype,
+            trainable=True
+        )
+
+        # trainable input eigenvalues
+
+        self.eig_in = self.add_weight(
+            name='eig_in',
+            shape=(1, input_shape[-1]),
+            initializer=self.eig_in_initializer,
+            regularizer=self.eig_in_regularizer,
+            constraint=self.eig_in_constraint,
+            dtype=self.dtype,
+            trainable=self.is_eig_in_trainable
+        )
+        # trainable output eigenvalues
+
+        self.eig_out = self.add_weight(
+            name='eig_out',
+            shape=(self.units, 1),
+            initializer=self.eig_out_initializer,
+            regularizer=self.eig_out_regularizer,
+            constraint=self.eig_out_constraint,
+            dtype=self.dtype,
+            trainable=self.is_eig_out_trainable
+        )
+        # bias
+
+        if self.use_bias:
+
+            self.bias = self.add_weight(
+                name='bias',
+                shape=(1, self.units),
+                initializer=self.bias_initializer,
+                regularizer=self.bias_regularizer,
+                constraint=self.bias_constraint,
+                dtype=self.dtype,
+                trainable=True)
+        else:
+            self.bias = None
+
+        self.built = True
+
+    def call(self, inputs, **kwargs):
+
+        # phi = mat_mul(self.r, self.q)  # QR trasposto
+        phi = mul(self.eig_in - self.eig_out, mat_mul(self.r, self.q))
+
+        if self.dynamic_sparse:
+            self.control = tfp.stats.percentile(abs(phi), self.perc)
+
+        self.lin = sparsify(phi, self.control)
+
+        if self.use_bias:
+            action = mat_mul(inputs, transpose(self.lin)) + self.bias
+        else:
+            action = mat_mul(inputs, transpose(self.lin))
+
+        return self.activation(action)
+
+
+@keras_export('keras.layers.Sparse_Dense')
+class Sparse_Dense(Dense):
+    def __init__(self,
+                 percentile=100,
+                 dynamic_sparse=False,
+                 **kwargs):
+        super(Sparse_Dense, self).__init__(**kwargs)
+
+        self.perc = percentile
+        self.dynamic_sparse = dynamic_sparse
+
+    def build(self, input_shape):
+        super(Sparse_Dense, self).build(input_shape)
+
+        self.control = tfp.stats.percentile(abs(self.kernel), self.perc)
+
+    def call(self, inputs):
+        if self.dynamic_sparse:
+            self.control = tfp.stats.percentile(abs(self.kernel), self.perc)
+
+        self.w = sparsify(self.kernel, self.control)
+
+        return dense_action(
+            inputs,
+            self.w,
+            self.bias,
+            self.activation,
+            dtype=self._compute_dtype_object)
---
+++ Example/sparse_functions.py
@@ -0,0 +1,180 @@
+import tensorflow as tf
+import os
+import pickle as pk
+import numpy as np
+from os.path import join
+
+
+def QR_conf(units=1000,
+            activation='tanh',
+            is_eig_in_trainable=True,
+            is_eig_out_trainable=True,
+            use_bias=True
+            ):
+
+    return {'units': units,
+            'activation': activation,
+            'is_eig_in_trainable': is_eig_in_trainable,
+            'is_eig_out_trainable': is_eig_out_trainable,
+            'use_bias': use_bias
+            }
+
+def QR_confNC(units=1000,
+            activation='tanh',
+            is_eig_in_trainable=False,
+            is_eig_out_trainable=True,
+            use_bias=True
+            ):
+    return {'units': units,
+            'activation': activation,
+            'eig_in_initializer': 'ones',
+            'is_eig_in_trainable': is_eig_in_trainable,
+            'is_eig_out_trainable': is_eig_out_trainable,
+            'use_bias': use_bias
+            }
+
+
+def load_dataset(config, grup_channels=True):
+    if config['dataset'] == 'CIFAR10':
+        dataset = tf.keras.datasets.cifar10
+        in_shape = 32 * 32 * 3
+    elif config['dataset'] == 'F-MNIST':
+        dataset = tf.keras.datasets.fashion_mnist
+        in_shape = 28 * 28
+    elif config['dataset'] == 'MNIST':
+        dataset = tf.keras.datasets.mnist
+        in_shape = 28 * 28
+
+    if grup_channels:
+        o = 'F'
+    else:
+        o = 'C'
+
+    (x_train, y_train), (x_test, y_test) = dataset.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+
+    flat_train = np.reshape(x_train, [x_train.shape[0], in_shape], order=o)  # Tutto dataset
+    flat_test = np.reshape(x_test, [x_test.shape[0], in_shape], order=o)  # Tutto testset
+    return (flat_train, y_train), (flat_test, y_test)
+
+def sparse_model_build(config):
+    model = tf.keras.Sequential()
+    if config['dataset'] == 'CIFAR10':
+        model.add(tf.keras.Input(shape=(32 * 32 * 3,)))
+    else:
+        model.add(tf.keras.Input(shape=(28 * 28,)))
+
+    if config['type'] == 'QR':
+
+        hid_parameters = QR_conf(units=config['n2'])
+        last_parameters = QR_conf(units=10, activation='softmax')
+        for i in range(config['hidden_layers']):
+            model.add(QR(**hid_parameters, density=config['density']))
+        model.add(QR(**last_parameters, density=config['density']))
+
+        return model
+
+    elif config['type'] == 'QR_sparse':
+
+        hid_parameters = QR_conf(units=config['n2'])
+        last_parameters = QR_conf(units=10, activation='softmax')
+
+        for i in range(config['hidden_layers']):
+            model.add(QR_sparse(**hid_parameters,
+                                dynamic_sparse=config['dyn_sparse'],
+                                percentile=config['percentile']))
+        model.add(QR_sparse(**last_parameters,
+                            dynamic_sparse=config['dyn_sparse'],
+                            percentile=config['percentile']))
+
+        return model
+
+    elif config['type'] == 'QR_sparseNC':
+
+        first_parameters = QR_conf(units=config['n2'])
+        hid_parameters = QR_confNC(units=config['n2'])
+        last_parameters = QR_conf(units=10, activation='softmax')
+
+        model.add(QR_sparse(**first_parameters,
+                            dynamic_sparse=config['dyn_sparse'],
+                            percentile=config['percentile']))
+        for i in range(config['hidden_layers']-1):
+            model.add(QR_sparse(**hid_parameters,
+                                dynamic_sparse=config['dyn_sparse'],
+                                percentile=config['percentile']))
+        model.add(QR_sparse(**last_parameters,
+                            dynamic_sparse=config['dyn_sparse'],
+                            percentile=config['percentile']))
+
+        return model
+
+    elif config['type'] == 'Dense_sparse':
+
+        for i in range(config['hidden_layers']):
+            model.add(Sparse_Dense(units=config['n2'],
+                                   activation='tanh',
+                                   use_bias=False,
+                                   dynamic_sparse=config['dyn_sparse'],
+                                   percentile=config['percentile']))
+
+        model.add(Sparse_Dense(units=10,
+                               activation="softmax",
+                               use_bias=False,
+                               dynamic_sparse=config['dyn_sparse'],
+                               percentile=config['percentile']))
+        return model
+
+    elif config['type'] == 'Dense':
+        for i in range(config['hidden_layers']):
+            model.add(tf.keras.layers.Dense(units=config['n2'],
+                                            activation='tanh',
+                                            use_bias=True))
+
+        model.add(tf.keras.layers.Dense(units=10,
+                                        activation="softmax",
+                                        use_bias=True))
+        return model
+
+def training(configur):
+
+    if configur['dataset'].find('CIFAR') != -1:
+        lr = {'Dense': 0.0001, 'Dense_sparse': 0.0001, 'QR': 0.003, 'QR_sparse': 0.003}
+    elif configur['dataset']=='F-MNIST':
+        lr = {'Dense': 0.001, 'Dense_sparse': 0.001, 'QR': 0.001, 'QR_sparse': 0.001}
+    elif configur['dataset']=='MNIST':
+        lr = {'Dense': 0.005, 'Dense_sparse': 0.005, 'QR': 0.005, 'QR_sparse': 0.005}
+
+    (x, y), (xtest, ytest) = load_dataset(configur)
+
+    model = sparse_model_build(configur)
+
+    stop_early = tf.keras.callbacks.EarlyStopping(monitor='val_accuracy',
+                                                  min_delta=0.005,
+                                                  mode='max',
+                                                  patience=5)
+
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=lr[configur['type']]),
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'],
+                  run_eagerly=False)
+
+    model.fit(x, y,
+              validation_split=0.3,
+              epochs=configur['epochs'],
+              batch_size=configur['batch_size'],
+              callbacks=stop_early,
+              verbose=0)
+
+    R = model.evaluate(xtest, ytest, batch_size=1000, verbose=1)
+    folder = join(configur['path'], configur['dataset'])
+    os.makedirs(folder, exist_ok=True)
+
+    #Dynamic sparse
+    n = configur['type']+'_'+str(configur['percentile'])#configur['type']+'{:.3f}'.format(configur['density'])+'.p' #str(configur['percentile'])
+    name = join(folder, n)
+
+    ##QR accuracy
+    # name = os.path.join(folder, configur['type'] + str(configur['n2']) + '.p')
+
+    with open(name, "ab") as f:
+        pk.dump(R[1], f)
